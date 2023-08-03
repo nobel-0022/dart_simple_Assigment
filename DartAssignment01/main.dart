5ed9c45056336a10b28ef2f70b9b4153e2a9ed5a
nobel-0022 <83616937+nobel-0022@users.noreply.github.com>
2023-08-04 01:06:17 +0600

    Add files via upload

---
+++ DartAssignment01/main.dart
@@ -0,0 +1,24 @@
+void main() {
+  var a = 7;
+  var b = 3;
+
+  //Addition
+  var addition = 7 + 3;
+  print(addition);
+
+  //Subtraction
+  var subtraction = 7 - 3;
+  print(subtraction);
+
+  //Multiplication
+  var multiplication = 7 * 3;
+  print(multiplication);
+
+  //Division
+  var division = 7 / 3;
+  print(division);
+
+  // Modulus
+  var modulus = 7 % 3;
+  print(modulus);
+}
